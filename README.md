2ac15b0ef4ba431ad04179b84b65624871f921ff
benlambert43 <benlambert43@gmail.com>
2020-06-24 20:37:27 +0100

    created webserver

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # enviroPlus_Device
 
 Device application driver for enviro+ hardware.
+
+Web server for exporting enviro+ values with a REST API
--- test1.py
+++ test1.py
@@ -76,7 +76,7 @@ try:
         # Text settings.
         font_size = 25
         text_colour = (255, 255, 255)
-        back_colour = (0, 170, 170)
+        back_colour = (0, 0, 0)
 
         message = "temp = " + str(temperature) + "\n" + \
             "press = " + str(pressure) + "\n" + "hum = " + \
